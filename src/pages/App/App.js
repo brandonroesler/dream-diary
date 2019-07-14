b3fb36b721c7b3bd77d880036f474cbcd8de4f17
brandonroesler <roesler19@gmail.com>
2019-07-14 16:10:30 -0500

    continuing auth

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import LoginPage from '../LoginPage/LoginPage';
 import SignupPage from '../SignupPage/SignupPage';
 // import LoginPage from '../LoginPage/LoginPage';
-// import userService from '../../utils/userService';
+import userService from '../../utils/userService';
 // import tokenService from '../../utils/tokenService';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      user: userService.getUser()
+    }
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: userService.getUser()})
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
   render() {
     return (
       <div className='App'>
@@ -17,12 +34,19 @@ class App extends Component {
           <Route exact path='/' render={() =>
             <HomePage />
           } />
-          <Route exact path='/login' render={() =>
-            <LoginPage />
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
           }/>
-          <Route exact path='/signup' render={() =>
-            <SignupPage />
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
           }/>
+
         </Switch>
       </div>
     )
