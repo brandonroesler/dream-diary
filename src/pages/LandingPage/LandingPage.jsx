7a94d5a7b79ef91e59ceb6f6915754b1367f1b04
brandonroesler <roesler19@gmail.com>
2019-07-17 13:12:49 -0500

    final touches

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './LandingPage.css'
+import clouds from './bg-clouds.jpg'
 
 class LandingPage extends React.Component {
     render() {
         return(
             <div>
-                <h1>Welcome to the Landing Page!</h1>
+                <h1>Dream Diary</h1>
+                <h4>Record your dreams <br></br>to control your dreams!</h4>
+            
             </div>
         )
     }
